8d118e96dfc881b99918544c0d2f133325f0b0ac
Cooper Anderson <cooper@cooperstuff.ninja>
2018-07-04 21:09:30 -0700

    Stop player from picking up items in inventories
    Force the player to drop any items that do not match the filter. If my pull request on MinecraftForge gets accepted then this can be done in a better way.

--- src/main/java/ninja/cooperstuff/inventoryfilter/FilterEventHandler.java
+++ src/main/java/ninja/cooperstuff/inventoryfilter/FilterEventHandler.java
@@ -1,13 +1,27 @@
 package ninja.cooperstuff.inventoryfilter;
 
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 public class FilterEventHandler {
 	@SubscribeEvent
 	public void onItemPickup(EntityItemPickupEvent event) {
 		if (FilterConfig.enabled && InventoryFilter.isGamemodeEnabled(event.getEntityPlayer())) {
 			event.setCanceled(!InventoryFilter.isItemAllowed(event.getItem().getItem().getItem()));
+		}
+	}
+
+	@SubscribeEvent
+	public void onInventoryChange(TickEvent.PlayerTickEvent event) {
+		for (int i = 0; i < event.player.inventory.getSizeInventory(); i++) {
+			ItemStack itemStack = event.player.inventory.getStackInSlot(i);
+			if (FilterConfig.enabled && InventoryFilter.isGamemodeEnabled(event.player) && itemStack.getItem() != Items.AIR && !InventoryFilter.isItemAllowed(itemStack.getItem())) {
+				event.player.dropItem(itemStack, true);
+				event.player.inventory.removeStackFromSlot(i);
+			}
 		}
 	}
 }
--- src/main/java/ninja/cooperstuff/inventoryfilter/InventoryFilter.java
+++ src/main/java/ninja/cooperstuff/inventoryfilter/InventoryFilter.java
@@ -17,7 +17,7 @@ public class InventoryFilter
 {
     public static final String MODID = "inventoryfilter";
     public static final String NAME = "Inventory Filter";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.2";
 
     public static Logger logger;
 
